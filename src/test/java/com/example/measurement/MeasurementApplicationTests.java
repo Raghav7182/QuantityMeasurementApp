6a46a6fa358adbd26d926c39c51dd8c291f70cab
Raghav <raghav7182.ca22@chitkara.edu.in>
2026-05-12 10:51:46 +0530

    UC2: Inches equality implemented

--- src/test/java/com/example/measurement/MeasurementApplicationTests.java
+++ src/test/java/com/example/measurement/MeasurementApplicationTests.java
@@ -4,28 +4,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
-//@SpringBootTest
 class MeasurementApplicationTests {
+
 	@Test
-	void testFeetEquality_SameValue() {
+	void testFeetEquality_SameValue(){
 		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
 		MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(89.6);
+
 		assertEquals(f1, f2);
 	}
-
 	@Test
-	void testFeetInEquality_DifferValue() {
+	void testFeetEquality_DifferValue(){
 		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
 		MeasurementApplication.Feet f2 = new MeasurementApplication.Feet(34.6);
-		assertNotEquals(f1, f2);
-	}
 
+		assertNotEquals(f1,f2);
+	}
 	@Test
-	void testFeetNullable_NullValue() {
+	void testFeetNullable_NullValue(){
 		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(55.0);
+
 		assertFalse(f1.equals(null));
 	}
-
 	@Test
 	void testFeetEquality_ClassComparison(){
 		MeasurementApplication.Feet f1 = new MeasurementApplication.Feet(89.6);
@@ -38,9 +38,39 @@ class MeasurementApplicationTests {
 
 		assertTrue(f1.equals(f1));
 	}
-
 	@Test
-	void contextLoads() {
+	void testInchEquality_SameValue(){
+		MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(12.5);
+		MeasurementApplication.Inch i2 = new MeasurementApplication.Inch(12.5);
+
+		assertEquals(i1, i2);
 	}
 
+	@Test
+	void testInchEquality_DifferValue(){
+		MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(12.5);
+		MeasurementApplication.Inch i2 = new MeasurementApplication.Inch(10.0);
+
+		assertNotEquals(i1, i2);
+	}
+
+	@Test
+	void testInchNullable_NullValue(){
+		MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(20.0);
+
+		assertFalse(i1.equals(null));
+	}
+
+	@Test
+	void testInchEquality_ClassComparison(){
+		MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(15.0);
+
+		assertFalse(i1.equals("Some String"));
+	}
+	@Test
+	void testInchEquality_SameReference(){
+		MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(18.0);
+
+		assertTrue(i1.equals(i1));
+	}
 }
